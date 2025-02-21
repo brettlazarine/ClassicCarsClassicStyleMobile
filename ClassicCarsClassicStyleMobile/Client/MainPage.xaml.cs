bd53589112f6e8035ac9e2eb23a897eb5a366528
Brett Lazarine <jblazarine@gmail.com>
2025-02-21 12:48:15 -0600

    create Bluetooth scanner

--- ClassicCarsClassicStyleMobile/Client/MainPage.xaml.cs
+++ ClassicCarsClassicStyleMobile/Client/MainPage.xaml.cs
@@ -1,23 +1,58 @@
-﻿namespace Client;
+﻿
+using System.Diagnostics;
+using Shiny.BluetoothLE;
+
+namespace Client;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
-    public MainPage()
+    private readonly IBleManager _bleManager;
+    public List<ScanResult> Results { get; } = new();
+    
+    public MainPage(IBleManager bleManager)
     {
+        _bleManager = bleManager;
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    protected override void OnAppearing()
     {
-        count++;
+        base.OnAppearing();
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+        try
+        {
+            Scan();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("***** MainPage OnAppearing() *****");
+            Debug.WriteLine(ex.Message);
+            Debug.WriteLine("********************");
+        }
+    }
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+    public void Scan()
+    {
+        if (!_bleManager.IsScanning)
+        {
+            _bleManager.StopScan();
+        }
+        
+        Results.Clear();
+
+        _bleManager.Scan()
+            .Subscribe(_result =>
+            {
+                try
+                {
+                    Results.Add(_result);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("***** MainPage Scan() *****");
+                    Debug.WriteLine(ex.Message);
+                    Debug.WriteLine("********************");
+                }
+            });
     }
 }
